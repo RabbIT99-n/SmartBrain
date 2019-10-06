d032a7a57150deb6bff62dc359c7c8101b08319e
unknown <naida.dozic@gmail.com>
2019-10-06 16:22:40 +0200

    Updates

---
+++ precache-manifest.f1ff4983ccf3ead2e1bc7726190bcbf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601a43f7a6adf0150f3417ec27aac958",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "02cdda7c5cec942ca994",
+    "url": "/SmartBrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fd39ca77e53d161c83e6",
+    "url": "/SmartBrain/static/css/main.a6f5dc3b.chunk.css"
+  },
+  {
+    "revision": "02cdda7c5cec942ca994",
+    "url": "/SmartBrain/static/js/2.00a8ded7.chunk.js"
+  },
+  {
+    "revision": "fd39ca77e53d161c83e6",
+    "url": "/SmartBrain/static/js/main.66900141.chunk.js"
+  },
+  {
+    "revision": "c9aa9a0b2af8227eeb05",
+    "url": "/SmartBrain/static/js/runtime-main.d43bc3da.js"
+  }
+]);
